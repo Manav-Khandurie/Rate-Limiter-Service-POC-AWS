6562b6b36f5bea0640e8845fa854888b2aaf8201
Aditya-567 <lextrone000567@gmail.com>
2024-04-21 04:03:21 +0530

    dashboard

--- Client/src/pages/About.js
+++ Client/src/pages/About.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import one from "../assets/1.png"
+import two from "../assets/2.png"
 
 function About() {
     return (
-        <div>About</div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+
+            <div className='text-white' style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                <img src={one} alt="Logo" width={300} height={300} loading="lazy" />
+                <h3 >iufnarjibfiusarhfoieanfiuenoigfneraoighe4uigjiperbgouiegouipran<br />
+                    wtb4wuifobwiubfiuw  nfoiuwn iunhiofnoinw33oui</h3>
+            </div>
+            <div className='text-white' style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+
+                <h3>iufnarjibfiusarhfoieanfiuenoigfneraoighe4uigjiperbgouiegouipran<br />
+                    wtb4wuifobwiubfiuw  nfoiuwn iunhiofnoinw33oui</h3>
+                <img src={two} alt="Logo" width={300} height={300} loading="lazy" />
+            </div>
+
+
+
+        </div>
     )
 }
 
